d329651fae26e7825fc82e5fa45d2ee235da929e
Furqan Feroz <mfurqanlohiya@gmail.com>
2020-11-16 19:09:27 +0500

    Related Component without delete

--- TanmiahCloneWithDatabase/Controllers/RelatedItemsController.cs
+++ TanmiahCloneWithDatabase/Controllers/RelatedItemsController.cs
@@ -8,22 +8,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TanmiahCloneWithDatabase.Models;
+using TanmiahCloneWithDatabase.Services;
 
 namespace TanmiahCloneWithDatabase.Controllers
 {
     public class RelatedItemsController : Controller
     {
+        SqlCommand sqlCommand;
         // GET: RelatedItems
         public ActionResult Index()
         {
+            GetRelatedItems getItems = new GetRelatedItems();
             DataTable dataTable = new DataTable();
-            SqlDataAdapter sqlDataAdapter;
-            using (SqlConnection conn = new SqlConnection(SqlConn.ConnectionString))
-            {
-                conn.Open();
-                sqlDataAdapter = new SqlDataAdapter("Select * from RelatedItems", conn);
-                sqlDataAdapter.Fill(dataTable);
-            }
+            dataTable = getItems.GetRelatedItemsData();
             return PartialView("_RelatedItems", dataTable);
         }
 
@@ -41,18 +38,29 @@ namespace TanmiahCloneWithDatabase.Controllers
 
         // POST: RelatedItems/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(RelatedItemsEditModel relatedItemsEditModel, HttpPostedFileBase Image)
         {
+            CreateItems createItems = new CreateItems();
+
+            sqlCommand = new SqlCommand();
             try
             {
-                // TODO: Add insert logic here
+                if (Image != null)
 
-                return RedirectToAction("Index");
+                {
+                    string filename = System.IO.Path.GetFileName(Image.FileName);
+                    string path = System.IO.Path.Combine(Server.MapPath("~/UploadedFiles"), filename);
+                    relatedItemsEditModel.Image = Image.FileName;
+                    Image.SaveAs(path);
+                }
+                ViewBag.FileStatus = "File uploaded successfully.";
+                sqlCommand = createItems.CreateHeaderData(relatedItemsEditModel);
             }
-            catch
+            catch (Exception)
             {
-                return View();
+                ViewBag.FileStatus = "Error while file uploading.";
             }
+            return RedirectToAction("Index", "Home");
         }
 
         [HttpGet]
@@ -60,45 +68,38 @@ namespace TanmiahCloneWithDatabase.Controllers
         public ActionResult Edit(int id)
         {
             RelatedItemsEditModel relatedItemsEditModel = new RelatedItemsEditModel();
-            DataTable dataTableEdit = new DataTable();
-            using (SqlConnection con = new SqlConnection(SqlConn.ConnectionString))
+            RelatedItemsService relatedItemsService = new RelatedItemsService();
+            relatedItemsEditModel = relatedItemsService.FillData(id);
+            if (relatedItemsEditModel != null)
             {
-                con.Open();
-                string editQuery = "Select * from RelatedItems where ID = @ID";
-                SqlDataAdapter sqlDataAdapterEdit = new SqlDataAdapter(editQuery, con);
-                sqlDataAdapterEdit.SelectCommand.Parameters.AddWithValue("@ID", id);
-                sqlDataAdapterEdit.Fill(dataTableEdit);
-
-            }
-            if (dataTableEdit.Rows.Count == 1)
-            {
-                relatedItemsEditModel.ID = Convert.ToInt32(dataTableEdit.Rows[0][0].ToString());
-                relatedItemsEditModel.Image = dataTableEdit.Rows[0][1].ToString();
-                relatedItemsEditModel.Name = dataTableEdit.Rows[0][2].ToString();
-                relatedItemsEditModel.Title = dataTableEdit.Rows[0][3].ToString();
-                relatedItemsEditModel.Description = dataTableEdit.Rows[0][4].ToString();
                 return View(relatedItemsEditModel);
             }
-
             return RedirectToAction("Index");
         }
 
         // POST: RelatedItems/Edit/5
         [HttpPost]
-        public ActionResult Edit(RelatedItemsEditModel relatedItemsEditModel)
+        public ActionResult Edit(RelatedItemsEditModel relatedItemsEditModel, HttpPostedFileBase Image) 
         {
-            using (SqlConnection con = new SqlConnection(SqlConn.ConnectionString))
+            UpdateItems updateItem = new UpdateItems();
+            string type = "Update";
+            sqlCommand = new SqlCommand();
+            try
             {
-                con.Open();
-                string updateQuery = "Update RelatedItems Set RelatedImage = @Image, RelatedName = @Name, RelatedTitle = @Title, RelatedDesc = @Description where ID = @ID";
-                SqlCommand sqlCommand = new SqlCommand(updateQuery, con);
-                sqlCommand.Parameters.AddWithValue("@ID", relatedItemsEditModel.ID);
-                sqlCommand.Parameters.AddWithValue("@Image", relatedItemsEditModel.Image);
-                sqlCommand.Parameters.AddWithValue("@Name", relatedItemsEditModel.Name);
-                sqlCommand.Parameters.AddWithValue("@Title", relatedItemsEditModel.Title);
-                sqlCommand.Parameters.AddWithValue("@Description", relatedItemsEditModel.Description);
-                sqlCommand.ExecuteNonQuery();
+                if (Image != null)
 
+                {
+                    string filename = System.IO.Path.GetFileName(Image.FileName);
+                    string path = System.IO.Path.Combine(Server.MapPath("~/UploadedFiles"), filename);
+                    relatedItemsEditModel.Image = Image.FileName;
+                    Image.SaveAs(path);
+                }
+                ViewBag.FileStatus = "File uploaded successfully.";
+                sqlCommand = updateItem.UpdateItemsData(relatedItemsEditModel, type);
+            }
+            catch (Exception)
+            {
+                ViewBag.FileStatus = "Error while file uploading.";
             }
             return RedirectToAction("Index", "Home");
         }
---
+++ TanmiahCloneWithDatabase/Services/RelatedItemsService.cs
@@ -0,0 +1,118 @@
+﻿using SqlItmes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using TanmiahCloneWithDatabase.Models;
+
+namespace TanmiahCloneWithDatabase.Services
+{
+    public class RelatedItemsService
+    {
+        GetRelatedItems getRelatedItems = new GetRelatedItems();
+        RelatedItemsEditModel relatedItemsEditModel = new RelatedItemsEditModel();
+        DataTable dataTable;
+
+        public RelatedItemsEditModel FillData(int id)
+        {
+            dataTable = new DataTable();
+            dataTable = getRelatedItems.GetRelatedItemsData(id);
+            if (dataTable.Rows.Count == 1)
+            {
+                relatedItemsEditModel.ID = Convert.ToInt32(dataTable.Rows[0][0].ToString());
+                relatedItemsEditModel.Image = dataTable.Rows[0][1].ToString();
+                relatedItemsEditModel.Name = dataTable.Rows[0][2].ToString();
+                relatedItemsEditModel.Title = dataTable.Rows[0][3].ToString();
+                relatedItemsEditModel.Description = dataTable.Rows[0][4].ToString();
+               
+                return relatedItemsEditModel;
+            }
+            return relatedItemsEditModel;
+        }
+    }
+    public class GetRelatedItems
+    {
+        SqlCommand sqlCommand;
+        SqlConnection sqlConnection;
+        SqlDataReader sqlDataReader;
+        DataTable dataTable;
+
+        public DataTable GetRelatedItemsData(int id)
+        {
+            using (sqlConnection = new SqlConnection(SqlConn.ConnectionString))
+            {
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand("SpMasterRelatedItems", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@StatementType", "SingleSelect");
+                sqlCommand.Parameters.AddWithValue("@ID", id);
+
+                sqlDataReader = sqlCommand.ExecuteReader();
+                dataTable = new DataTable();
+                dataTable.Load(sqlDataReader);
+                return dataTable;
+            }
+        }
+        public DataTable GetRelatedItemsData()
+        {
+            using (sqlConnection = new SqlConnection(SqlConn.ConnectionString))
+            {
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand("SpMasterRelatedItems", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@StatementType", "AllSelect");
+                sqlDataReader = sqlCommand.ExecuteReader();
+                dataTable = new DataTable();
+                dataTable.Load(sqlDataReader);
+                return dataTable;
+            }
+        }
+    }
+    public class CreateItems
+    {
+        SqlCommand sqlCommand;
+        SqlConnection sqlConnection;
+
+        public SqlCommand CreateHeaderData(RelatedItemsEditModel relatedItemsEditModel)
+        {
+            using (sqlConnection = new SqlConnection(SqlConn.ConnectionString))
+            {
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand("SpMasterRelatedItems", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@StatementType", "Insert");
+                sqlCommand.Parameters.AddWithValue("@RelatedTitle", relatedItemsEditModel.Title);
+                sqlCommand.Parameters.AddWithValue("@RelatedName", relatedItemsEditModel.Name);
+                sqlCommand.Parameters.AddWithValue("@RelatedDesc", relatedItemsEditModel.Description);
+                sqlCommand.Parameters.AddWithValue("@RelatedImage", relatedItemsEditModel.Image);
+                sqlCommand.ExecuteNonQuery();
+            }
+            return sqlCommand;
+        }
+    }
+    public class UpdateItems
+    {
+        SqlCommand sqlCommand;
+        SqlConnection sqlConnection;
+
+        public SqlCommand UpdateItemsData(RelatedItemsEditModel relatedItemsEditModel, string type)
+        {
+            using (sqlConnection = new SqlConnection(SqlConn.ConnectionString))
+            {
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand("SpMasterRelatedItems", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@StatementType", type);
+                sqlCommand.Parameters.AddWithValue("@ID", relatedItemsEditModel.ID);
+                sqlCommand.Parameters.AddWithValue("@RelatedTitle", relatedItemsEditModel.Title);
+                sqlCommand.Parameters.AddWithValue("@RelatedName", relatedItemsEditModel.Name);
+                sqlCommand.Parameters.AddWithValue("@RelatedDesc", relatedItemsEditModel.Description);
+                sqlCommand.Parameters.AddWithValue("@RelatedImage", relatedItemsEditModel.Image);
+                sqlCommand.ExecuteNonQuery();
+                return sqlCommand;
+            }
+        }
+    }
+}
